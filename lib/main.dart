29d190bb1b37fcf53e297e116205b80fa906037f
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-07-04 14:13:09 +0530

    build : Add dark theme feature and update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
